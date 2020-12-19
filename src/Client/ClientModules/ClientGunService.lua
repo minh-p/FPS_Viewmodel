c561f53b76dd2657c93262a55b97910cac99b92d
minh-p <minhisverynormal@gmail.com>
2020-12-19 15:40:53 -0800

    Basic Client FPS Framework
    Just getting started, no planning needed.

---
+++ src/Client/ClientModules/ClientGunService.lua
@@ -0,0 +1,21 @@
+-- Client Framework for Fps viewmodel
+-- 12/9/2020
+-- Minhnormal
+
+local ClientGunService = {}
+
+function ClientGunService.new()
+    
+end
+
+
+function ClientGunService:equipViewmodelGun()
+    
+end
+
+
+function ClientGunService:unequipViewmodelGun()
+    
+end
+
+return ClientGunService
