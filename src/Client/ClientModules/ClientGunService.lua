da207f953d222da71734529080a857c0d7ba36b7
minh-p <minhisverynormal@gmail.com>
2020-12-19 15:54:19 -0800

    Setting up Keybindings

--- src/Client/ClientModules/ClientGunService.lua
+++ src/Client/ClientModules/ClientGunService.lua
@@ -2,8 +2,18 @@
 -- 12/9/2020
 -- Minhnormal
 
+local ContextActionService = game:GetService("ContextActionService")
+
 local ClientGunService = {}
 ClientGunService.__index = ClientGunService
+
+ClientGunService.enumBinds = {
+    [1] = "One",
+    [2] = "Two",
+    [3] = "Three"
+}
+
+ClientGunService.currentWeapon = nil
 
 function ClientGunService.new()
     local self = {}
@@ -13,12 +23,16 @@ end
 
 
 function ClientGunService:bindEquipViewmodelGun()
-    
+    ContextActionService:BindAction(function()
+        
+    end)
 end
 
 
 function ClientGunService:bindUnequipViewmodelGun()
-    
+    ContextActionService:BindAction(function()
+        
+    end)
 end
 
 return ClientGunService
